e02bf892ee2e11697bd40e304fa16784ba6078d0
phocsoccute1412 <khanhnguyenkatee1412@gmail.com>
2023-04-13 16:54:30 +0700

    form sign up redirect done

--- src/pages/api/testbot1.ts
+++ src/pages/api/testbot1.ts
@@ -7,23 +7,32 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ){
-  try {
+  // try {
+    try{
     const database = client.db('hairnet')
     const users = database.collection('Users')
     const query = {name:'testbot1'}
-    const user = await users.find(query).toArray(function (err:any, result:any) {
-      if (err) {
-        console.log(err);
-      } else if (result.length) {
-        console.log('Found:', result);
-      } else {
-        console.log('No document(s) found with defined "find" criteria!');
-      }
-      //Close connection
-      database.close();
-    });
-    res.json(user)
-  } catch (err) {
-    console.log("error")
+    const results = await users.insertOne({
+      username: req.body.userName,
+      email: req.body.Email,
+      password: req.body.Phone
+    })
+    res.status(200)
+    res.redirect(307,'/')
   }
+  catch(err) {console.log(err)}
+  //   const user = await users.find(query).toArray(function (err:any, result:any) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else if (result.length) {
+  //       console.log('Found:', result);
+  //     } else {
+  //       console.log('No document(s) found with defined "find" criteria!');
+  //     }
+  //     //Close connection
+  //     database.close();
+  //   });
+  // } catch (err) {
+  //   console.log("error")
+  // }
 }
--- src/pages/login/form_signup.tsx
+++ src/pages/login/form_signup.tsx
@@ -1,20 +1,24 @@
 import homecss from '../../styles/homePage.module.css';
+import { NextApiRequest, NextApiResponse } from 'next';
+async function handle(req:NextApiRequest,res:NextApiResponse){
+    res.status(200);
+}
 export default function SignupForm(){
     return (
         <div className={homecss.homeSignUp}>
             <h1 className={homecss.signuptext}>Sign up to start</h1>
-            <form action="" className={homecss.form}>
+            <form action="/api/testbot1" className={homecss.form} method='POST'>
                 <div className={homecss.record}>
                     <label htmlFor="">User Name</label>
-                    <input type="text" className={homecss.blank1} />
+                    <input type="text" className={homecss.blank1} required name='userName'/>
                 </div>
                 <div className={homecss.record}>
                     <label htmlFor="">Email</label>
-                    <input type="text" className={homecss.blank2}/>
+                    <input type="text" className={homecss.blank2} required name='Email'/>
                 </div>
                 <div className={homecss.record}>
                     <label htmlFor="">Phone</label>
-                    <input type="text" className={homecss.blank3}/>
+                    <input type="text" className={homecss.blank3} name='Phone'/>
                 </div>
                 <div className={homecss.otherSignUpText}>
                     <button className={homecss.buttonSignup}>Submit</button>
