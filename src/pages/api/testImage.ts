7c016d44de720fdc90a01cd32848e8006a0354df
phocsoccute1412 <khanhnguyenkatee1412@gmail.com>
2023-04-27 00:15:12 +0700

    hoàn thành upload ảnh vào folder public

---
+++ src/pages/api/testImage.ts
@@ -0,0 +1,38 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import formidable from "formidable";
+import path from "path";
+import fs from "fs/promises";
+
+export const config = {
+  api: {
+    bodyParser:false
+  }
+}
+
+const readFile = (req:NextApiRequest, saveLocally?:boolean):Promise<{fields:formidable.Fields, files:formidable.Files}> =>{
+  const options:formidable.Options = {}
+  if(saveLocally){
+    options.uploadDir = path.join(process.cwd(),'/public/uploads')
+    options.filename = (name, ext, path, form) =>{
+      console.log(path.originalFilename)
+      return Date.now().toString() + '_' + path.originalFilename
+    }
+  }
+  const form = formidable(options)
+  return new Promise((resolve, reject) =>{
+    form.parse(req, (err, fields, files) =>{
+      if(err) reject(err)
+      resolve({fields, files})
+    })
+  })
+}
+
+const handler : NextApiHandler = async (req, res) => {
+  try {
+    await fs.readdir(path.join(process.cwd()+'/public', '/uploads'))
+  } catch (error) {
+    await fs.mkdir(path.join(process.cwd()+'/public', '/uploads'))
+  }
+  readFile(req, true)
+}
+export default handler
---
+++ src/pages/social/testuploadfile.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { NextPage } from "next";
+import {useState} from 'react'
+
+interface Props {}
+
+const Homee:NextPage<Props> =()=>{
+    const [uploading, setUploading] = useState(false)
+    const [selectedImage, setSelectedImage] = useState('')
+    const [selectedFile, setSelectedFile] = useState<File>()
+
+    const handleUpload = async () => {
+        setUploading(true)
+        try {
+            if(!selectedFile) return
+            const formData = new FormData()
+            formData.append('myImage', selectedFile)
+            const {data} = await axios.post("/api/testImage", formData)
+            console.log(data)
+        } catch (error:any) {
+            console.log(error.response?.data)
+        }
+        setUploading(false)
+    }
+    return (
+        <div>
+            <label htmlFor="">
+                <input type="file"
+                onChange={({target}) =>{
+                    if(target.files){
+                        const file = target.files[0]
+                        setSelectedImage(URL.createObjectURL(file))
+                        setSelectedFile(file)
+                    }
+                }}/>
+                <div>{selectedImage?(<img src={selectedImage} alt="" />):<span>Select Image</span>}</div>
+            </label>
+            <button onClick={handleUpload}>submit</button>
+        </div>
+    )
+}
+export default Homee
